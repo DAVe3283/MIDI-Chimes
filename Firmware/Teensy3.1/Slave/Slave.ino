4b030c02fc5ad6ddc35f512615cf5bb942ef3699
DAVe3283 <DAVe3283@gmail.com>
2016-02-06 09:50:42 -0700

    Update Slave firmware to match schematic.
    I ended up changing around the feedback inputs to clean up the
    PCB layout.

--- Firmware/Teensy3.1/Slave/Slave.ino
+++ Firmware/Teensy3.1/Slave/Slave.ino
@@ -26,8 +26,10 @@
 // Constants
 // -----------------------------------------------------------------------------
 
-// I2C config
-const uint8_t i2c_address('A');
+// I2C Configuration
+const uint8_t i2c_address('A'); // Temporary until auto-config address is done
+const uint8_t addr_latch_i_pin(1); // Address latch input pin
+const uint8_t addr_latch_o_pin(2); // Address latch output pin
 
 // PWM pins used by the chime coils
 const uint8_t chime_pins[] =
@@ -48,19 +50,22 @@ const uint8_t num_channels(sizeof(chime_pins) / sizeof(*chime_pins));
 // Analog pins used for feedback
 const uint8_t feedback_pins[num_channels] =
 {
-   A0, // Channel 1 (pin 14)
-   A1, // Channel 2 (pin 15)
-   A2, // Channel 3 (pin 16)
-   A3, // Channel 4 (pin 17)
-       // Pins 18-19 are I2C!
-  A15, // Channel 5 (on back)
-  A16, // Channel 6 (on back)
-  A17, // Channel 7 (on back)
-  A18, // Channel 8 (on back)
-  A19, // Channel 9 (on back)
-  A20, // Channel 10 (on back)
+  A12, // Channel 1 (on back)
+  A15, // Channel 2 (pin 26 on back)
+  A16, // Channel 3 (pin 27 on back)
+  A17, // Channel 4 (pin 28 on back)
+  A14, // Channel 5 (bottom row)
+   A0, // Channel 6 (pin 14)
+   A1, // Channel 7 (pin 15)
+   A2, // Channel 8 (pin 16)
+   A3, // Channel 9 (pin 17)
+  A19, // Channel 10 (pin 30 on back)
 };
 
+// Power supply enable pin
+const uint8_t ps_enable_pin(0);
+
+// LED pin
 const uint8_t led_pin(13);
 
 // Timeouts
